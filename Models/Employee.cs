c0eb27c562306dc84635089714695a1c8b91d60e
Maragatham Muthusamy <mahaa,shankar@gmail.com>
2022-07-17 21:06:24 -0400

    First checkin
    First check in

---
+++ Models/Employee.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+#nullable disable
+
+namespace ASP.NET_CORE5_MVC_CRUD.Models
+{
+    public class EmployeeValidator
+    {
+        [Required]
+        [MaxLength(30)]
+        [Display(Name = "Employee Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Employee Salary")]
+        public decimal Salary { get; set; }
+
+        [Required]
+        [Display(Name = "Joining Date")]
+        public decimal JoiningDate { get; set; }
+    }
+
+    [ModelMetadataType(typeof(EmployeeValidator))]
+    public partial class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Designation { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime JoiningDate { get; set; }
+    }
+}
---
+++ Models/SampleDBContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace ASP.NET_CORE5_MVC_CRUD.Models
+{
+    public partial class SampleDBContext : DbContext
+    {
+        public SampleDBContext(DbContextOptions<SampleDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.Property(e => e.Address)
+                    .HasMaxLength(250)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Designation)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.JoiningDate)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Salary).HasColumnType("decimal(18, 0)");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
